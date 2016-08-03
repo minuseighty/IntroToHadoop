ddaa66a755d606c7c9442027dea710f9bd61ae32
minuseighty <jessani.zainil@gmail.com>
2016-08-03 14:30:14 -0600

    Create P1Q2_mapper.py

---
+++ P1Q2_mapper.py
@@ -0,0 +1,12 @@
+# Find the monetary value for the highest individual sale for each separate store.
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split("\t")  # remove whitespace and split on tab
+    if len(data) == 6:
+        date, time, store, item, cost, payment = data  #
+        print "{0}\t{1}".format(store, cost)  # print by store and cost
