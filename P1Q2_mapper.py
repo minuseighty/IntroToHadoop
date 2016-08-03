82934019c229d5621a8f20d30e24fdbffe5e2aea
minuseighty <jessani.zainil@gmail.com>
2016-08-03 14:30:38 -0600

    Update P1Q2_mapper.py

--- P1Q2_mapper.py
+++ P1Q2_mapper.py
@@ -8,5 +8,5 @@ import sys
 for line in sys.stdin:
     data = line.strip().split("\t")  # remove whitespace and split on tab
     if len(data) == 6:
-        date, time, store, item, cost, payment = data  #
+        date, time, store, item, cost, payment = data  #assign individual values
         print "{0}\t{1}".format(store, cost)  # print by store and cost
